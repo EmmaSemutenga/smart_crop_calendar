b113c573ae2518ae419ab062c1c547e6ae2b250b
Emmanuel Semutenga <emzsema@gmail.com>
2021-11-12 14:39:17 +0300

    changes db url

--- crop/models.py
+++ crop/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Crop(models.Model):
-    name = models.CharField(max_length=20)
-    agro_ecological_zone = models.CharField(max_length=50)
-    season = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    agro_ecological_zone = models.CharField(max_length=100)
+    season = models.CharField(max_length=100)
     early_sowing_month = models.PositiveBigIntegerField()
     late_sowing_month = models.PositiveBigIntegerField()
-    sowing_value = models.CharField(max_length=10)
-    sowing_unit = models.CharField(max_length=20)
-    growing_period = models.CharField(max_length=20)
-    water_needed = models.CharField(max_length=10, null=True, blank=True)
+    sowing_value = models.CharField(max_length=100)
+    sowing_unit = models.CharField(max_length=100)
+    growing_period = models.CharField(max_length=100)
+    water_needed = models.CharField(max_length=100, null=True, blank=True)
     challenges = models.TextField()
 
     def __str__(self):
--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +82,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -133,3 +130,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+# This is new
+# options = DATABASES['default'].get('OPTIONS', {})
+# options.pop('sslmode', None)
