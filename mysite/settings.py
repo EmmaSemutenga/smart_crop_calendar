cc6c1044d94f3c0541cfa39ea1b9ac95f6928ca1
Emmanuel Semutenga <emzsema@gmail.com>
2021-11-12 15:39:49 +0300

    change local db to sqllite

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 import django_heroku
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
